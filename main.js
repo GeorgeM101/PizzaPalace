8e9d2ced39b370756f135eeb42a32824a53faa13
GeorgeM101 <george.mboya@student.moringaschool.com>
2021-11-28 15:01:20 +0300

    entered location field

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 var pizzaSize = document.getElementById("size")
+var pizzaToppings = document.getElementById("toppings")
+var pizzaCrust = document.getElementById("crusts")
+var location = document.getElementById("location")
+
 
 var toppingsType = ["Pepperoni", "Mushroom", "Extra Cheese", "Green pepper"]
  
-function addCart(){
-    if(size.value == "--Select--" || size.value == "Small" || size.value == "Medium" || size.value == "Large")
-    alert("select size");
-    else(
- 
-    )
-}; 
+function formInput(){
+    if(size.value == "");
+    alert("Select size")
+}else if (size == "Small"){
+    return 1000;
+  } else {
+    return 500
+  };
+    
+
+  function location(){
+      if (location.value== "")
+      alert("Enter location")
+  }
