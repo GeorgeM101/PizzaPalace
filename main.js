7fe04a6ef0d1a092abd833d79afca420744553f8
GeorgeM101 <george.mboya@student.moringaschool.com>
2021-11-28 13:01:56 +0300

    add select option

---
+++ main.js
@@ -0,0 +1,11 @@
+var pizzaSize = document.getElementById("size")
+
+var toppingsType = ["Pepperoni", "Mushroom", "Extra Cheese", "Green pepper"]
+ 
+function addCart(){
+    if(size.value == "--Select--" || size.value == "Small" || size.value == "Medium" || size.value == "Large")
+    alert("select size");
+    else(
+ 
+    )
+}; 
