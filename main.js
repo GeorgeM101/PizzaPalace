5202b6b9485f26f5ee7795500e3b1197fd03eac5
GeorgeM101 <george.mboya@student.moringaschool.com>
2021-11-29 12:04:10 +0300

    Added readme file

--- main.js
+++ main.js
@@ -1,8 +1,66 @@
-var pizzaSize = document.getElementById("size");
-var pizzaType = document.getElementById("")
+// var pizzaSize = document.getElementById("size");
+// var pizzaType = document.getElementById("")
 
 
-function displaySize(){
-    if (size.value == 'Large')
-    alert("1200")
-};
+// function displaySize(){
+//     if (size.value == 'Large')
+//     alert("1200")
+// };
+
+$('#AddToCart').on('click', function () {
+    var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
+    var finalAmount = amount * +$('#pizzaQuantity').val()
+    $('#subTotal').html(finalAmount)
+    $('#cardCart').fadeIn(1000)
+})
+$('#flexSwitchCheckChecked').on('change', function () {
+    if($('#flexSwitchCheckChecked').is(':checked')){
+        $('#deliveryDetails').fadeIn(1000)
+    }else{
+        $('#deliveryDetails').fadeOut(1000)
+    }
+})
+$('#btnCheckout').on('click', function () {
+    $('#cardCheckout').fadeIn(1000)
+    $('#orderSize').html($('#pizzaSize').find('option:selected').text())
+    var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
+    var finalAmount = amount * +$('#pizzaQuantity').val()
+    var orderAmount = finalAmount
+    if($('#flexSwitchCheckChecked').is(':checked')){
+        orderAmount = finalAmount + 250
+    }
+    $('#orderTotal').html(orderAmount)
+})
+$('#btnCheckout').on('click',function(){
+    $('#cardCheckout').fadeIn(1000)
+    $('#orderCrust').html($('#pizzaCrust').find('option:selected').text())
+    var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
+    var finalAmount = amount * +$('#pizzaQuantity').val()
+    var orderAmount = finalAmount
+    if($('#flexSwitchCheckChecked').is(':checked')){
+        orderAmount = finalAmount + 1000
+    }
+    $('#orderTotal').html(orderAmount)
+})
+$('#btnCheckout').on('click',function(){
+    $('#cardCheckout').fadeIn(1000)
+    $('#orderTopping').html($('#pizzaTopping').find('option:selected').text())
+    var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
+    var finalAmount = amount * +$('#pizzaQuantity').val()
+    var orderAmount = finalAmount
+    if($('#flexSwitchCheckChecked').is(':checked')){
+        orderAmount = finalAmount + 1000
+    }
+    $('#orderTotal').html(orderAmount)
+})
+$('#btnCheckout').on('click',function(){
+    $('#cardCheckout').fadeIn(1000)
+    $('#orderNumber').html($('#pizzaQuantity').find('option:selected').text())
+    var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
+    var finalAmount = amount * +$('#pizzaQuantity').val()
+    var orderAmount = finalAmount
+    if($('#flexSwitchCheckChecked').is(':checked')){
+        orderAmount = finalAmount + 1000
+    }
+    $('#orderTotal').html(orderAmount)
+})
