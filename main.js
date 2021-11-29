fb1b347bc68b5ff83eff24c2bd9a1a5834d2ad76
GeorgeM101 <george.mboya@student.moringaschool.com>
2021-11-29 10:49:10 +0300

    Added jquery link

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-// var pizzaSize = document.getElementById("size")
-// var pizzaToppings = document.getElementById("toppings")
-// var pizzaCrust = document.getElementById("crusts")
-// var location = document.getElementById("location")
+var pizzaSize = document.getElementById("size");
+var pizzaType = document.getElementById("")
 
 
- 
-// function displaySize(e){
-//     e.preventDefault()
-//     if(size.value == "");
-//     alert("Select size")
-// }if (size.value == "Small"){
-//     alert("1000")
-//   } else if(size.value == "Medium") {
-//     alert("800")
-//   }else(size.value == "Large");{
-//       alert(1200)
-//   };
-    
-
-  function setPlace(){
-      if (place.value== "")
-      alert("Enter location")
-  };
+function displaySize(){
+    if (size.value == 'Large')
+    alert("1200")
+};
