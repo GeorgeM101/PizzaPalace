0616bffd881236a72a2fb3c2fa450d770a3a6f64
GeorgeM101 <george.mboya@student.moringaschool.com>
2021-11-29 16:02:59 +0300

    Added description on readme

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# DESCRITPION
+
+Pizza Palace is a resturant that allows you to order your favorite kind of pizza at the comfort of your house.
--- main.js
+++ main.js
@@ -1,28 +1,21 @@
-// var pizzaSize = document.getElementById("size");
-// var pizzaType = document.getElementById("")
-
-
-// function displaySize(){
-//     if (size.value == 'Large')
-//     alert("1200")
-// };
 
 $('#AddToCart').on('click', function (event) {
     event.preventDefault();
     var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
     var finalAmount = amount * +$('#pizzaQuantity').val()
     $('#subTotal').html(finalAmount)
-    $('#cardCart').fadeIn(1000)
+    $('#cardCart').fadeIn(100)
 })
 $('#flexSwitchCheckChecked').on('change', function () {
     if($('#flexSwitchCheckChecked').is(':checked')){
-        $('#deliveryDetails').fadeIn(1000)
+        $('#deliveryDetails').fadeIn(100)
     }else{
-        $('#deliveryDetails').fadeOut(1000)
+        $('#deliveryDetails').fadeOut(100)
     }
 })
-$('#btnCheckout').on('click', function () {
-    $('#cardCheckout').fadeIn(1000)
+$('#checkOut').on('click', function (event) {
+    event.preventDefault();
+    $('#checkoutBox').fadeIn(1000)
     $('#orderSize').html($('#pizzaSize').find('option:selected').text())
     var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
     var finalAmount = amount * +$('#pizzaQuantity').val()
@@ -32,30 +25,30 @@ $('#btnCheckout').on('click', function () {
     }
     $('#orderTotal').html(orderAmount)
 })
-$('#btnCheckout').on('click',function(){
-    $('#cardCheckout').fadeIn(1000)
+$('#checkOut').on('click',function(){
+    $('#checkoutBox').fadeIn(1000)
     $('#orderCrust').html($('#pizzaCrust').find('option:selected').text())
     var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
     var finalAmount = amount * +$('#pizzaQuantity').val()
     var orderAmount = finalAmount
     if($('#flexSwitchCheckChecked').is(':checked')){
-        orderAmount = finalAmount + 1000
+        orderAmount = finalAmount + 150
     }
     $('#orderTotal').html(orderAmount)
 })
-$('#btnCheckout').on('click',function(){
-    $('#cardCheckout').fadeIn(1000)
+$('#checkOut').on('click',function(){
+    $('#checkoutBox').fadeIn(1000)
     $('#orderTopping').html($('#pizzaTopping').find('option:selected').text())
     var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
     var finalAmount = amount * +$('#pizzaQuantity').val()
     var orderAmount = finalAmount
     if($('#flexSwitchCheckChecked').is(':checked')){
-        orderAmount = finalAmount + 1000
+        orderAmount = finalAmount + 150
     }
     $('#orderTotal').html(orderAmount)
 })
-$('#btnCheckout').on('click',function(){
-    $('#cardCheckout').fadeIn(1000)
+$('#checkOut').on('click',function(){
+    $('##checkoutBox').fadeIn(1000)
     $('#orderNumber').html($('#pizzaQuantity').find('option:selected').text())
     var amount = +$('#pizzaSize').val() + +$('#pizzaCrust').val() + +$('#pizzaTopping').val()
     var finalAmount = amount * +$('#pizzaQuantity').val()
@@ -64,4 +57,4 @@ $('#btnCheckout').on('click',function(){
         orderAmount = finalAmount + 1000
     }
     $('#orderTotal').html(orderAmount)
-})
+});
